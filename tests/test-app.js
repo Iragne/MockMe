b5119079503821897b8cbdb6ec0152e2837c7091
Iragne <jai@netcosports.com>
2013-10-10 15:49:35 +0200

    init

---
+++ tests/test-app.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var assert = require("assert");
+var should = require('should');
+
+
+
+var port = 4242;
+
+var app = null;
+var sessionCookie = null;
+
+describe('API', function(){
+	describe('Models', function(){
+		before (function (done) {
+			done();
+		});
+		it('should exist TBD', function (done) {
+			done();
+		});
+	});
+});
